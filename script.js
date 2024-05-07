28762bbf019e444595aa83edd5884423befb35c9
danjpal16 <525dpalagano@frhsd.com>
2024-05-07 14:43:58 -0400

    more confusing javascript

--- script.js
+++ script.js
@@ -11,7 +11,40 @@ const teamMembers = [
         weaknesses: 'Fat',
         skills: ['Big Eater', 'Funny', 'Smart'],
         biography: 'Eric Cartman is a child',
-    }
+    },
+    {
+        name: 'Stan Marsh',
+        age: '10',
+        height: '4ft 4in',
+        weight: '90lbs',
+        purpose: 'Comic Relief',
+        strengths: 'Master Manipulator',
+        weaknesses: 'Fat',
+        skills: ['Big Eater', 'Funny', 'Smart'],
+        biography: 'Eric Cartman is a child',
+    },
+    {
+        name: 'Kenny McCormick',
+        age: '10',
+        height: '4ft 4in',
+        weight: '90lbs',
+        purpose: 'Comic Relief',
+        strengths: 'Master Manipulator',
+        weaknesses: 'Fat',
+        skills: ['Big Eater', 'Funny', 'Smart'],
+        biography: 'Eric Cartman is a child',
+    },
+    {
+        name: 'Kyle Broflovski',
+        age: '10',
+        height: '4ft 4in',
+        weight: '90lbs',
+        purpose: 'Comic Relief',
+        strengths: 'Master Manipulator',
+        weaknesses: 'Fat',
+        skills: ['Big Eater', 'Funny', 'Smart'],
+        biography: 'Eric Cartman is a child',
+    },
 ]
 
 function generateTeamCards(){
@@ -20,16 +53,37 @@ function generateTeamCards(){
 
     teamMembers.forEach(member => {
         const card = document.createElement('div')
-        card.classList.add('col-md-4')
+        card.classList.add('col-md-3')
+
+
+        let backgroundColor
+
+        switch(member.age.toLowerCase()) {
+            case '10':
+                backgroundColor = ""
+                break
+        }
+
+
+
+
+
+        card.style.backgroundColor = backgroundColor
+
+
+
+
+
+
 
         card.innerHTML= `
             <div class="card">
                 <div class="card-header">
                 ${member.name}
                 </div>
-                <p>
-                ${member.age}
-                </p>
+                <div class="card-body">
+                    <p><strong>Age:</strong> ${member.age}</p>
+                </div>
             </div>
         
         `
