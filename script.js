01f9ea568a99113c4d4c59c95f0df8f4171b4504
danjpal16 <144719377+danjpal16@users.noreply.github.com>
2024-05-29 17:48:05 -0400

    more java

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ const teamMembers = [
         height: '4ft 8in',
         weight: 'Overweight',
         purpose: 'Comic Relief',
-        strengths: ['Master Manipulator', 'Resourcefulness', 'Occasionally displays leadership qualities'],
-        weaknesses: ['Selfishness', 'Arrogance', 'Impulsiveness', 'Lack of empathy', 'Fat'],
-        skills: ['Big Eater', 'Funny', 'Smart', 'Persuasive speech', 'Strategic thinking'],
-        biography: 'Eric Cartman, a cunning and often malevolent fourth grader from South Park, Colorado, is known for his selfish antics, relentless pursuit of personal gain, and knack for stirring up controversy among his peers',
+        strengths: 'Master Manipulator, Resourcefulness, Occasionally displays leadership qualities',
+        weaknesses: 'Selfishness, Arrogance, Impulsiveness, Lack of empathy, Fat',
+        skills: 'Big Eater, Funny, Smart, Persuasive speech, Strategic thinking' ,
+        biography: 'Eric Cartman, the cunning and malevolent fourth grader from South Park, Colorado, is notorious for his selfish antics, relentless pursuit of personal gain, and talent for stirring controversy among his peers.',
         img: "ec1.png",
     },
     {
@@ -22,7 +22,7 @@ const teamMembers = [
         strengths: ['Empathy', 'Loyalty to friends', 'Strong sense of justice'],
         weaknesses: ['Naive', 'Easily influenced by peer pressure'],
         skills: ['Leadership', 'problem-solving', 'occasional musical talent'],
-        biography: 'Stan Marsh, a compassionate and principled fourth grader from South Park, Colorado, navigates the absurdities of life in the small mountain town alongside his close friends, often finding himself at the center of bizarre and comedic adventures',
+        biography: 'In South Park, Colorado, Stan Marsh, a compassionate fourth grader, embarks on comedic adventures with his close friends, navigating the towns absurdities.',
         img: "sm1.png",
     },
     {
@@ -34,7 +34,7 @@ const teamMembers = [
         strengths: 'Resilience, Loyalty to friends, Occasional acts of heroism',
         weaknesses: 'Muffled speech (due to his hood), Poor, Frequent unfortunate accidents',
         skills: ['Survival instincts', 'Street smarts', 'Resourcefulness'],
-        biography: 'Kenny McCormick, a perpetually hooded and ill-fated fourth grader from South Park, Colorado, endures a series of bizarre and often fatal mishaps, symbolizing the unpredictability and fragility of life in the eccentric mountain town',
+        biography: 'In South Park, Colorado, Kenny McCormick, a perpetually hooded fourth grader, embodies the unpredictability of life in the eccentric town through a series of bizarre and often fatal mishaps.',
         img: "km1.png",
     },
     {
@@ -46,7 +46,7 @@ const teamMembers = [
         strengths: 'Intelligence, Strong sense of justice, Loyalty to friends',
         weaknesses: 'Stubbornness, Tendency to overreact emotionally',
         skills: ['Critical thinking', 'Problem-solving', 'Occasional proficiency in public speaking'],
-        biography: 'Kyle Broflovski, a principled and intellectually curious fourth grader from South Park, Colorado, navigates the absurdities of life in the small mountain town alongside his friends, often confronting prejudice and injustice with unwavering determination',
+        biography: 'Kyle Broflovski, a principled and intellectually curious fourth grader from South Park, Colorado, confronts prejudice and injustice with unwavering determination as he navigates the absurdities of life in the small mountain town alongside his friends.',
         img: "kb1.png",
     },
     {
@@ -58,7 +58,7 @@ const teamMembers = [
         strengths: 'Kind-hearted, Resilience, Creativity',
         weaknesses: 'Naive, Gullible, Lack of assertiveness',
         skills: ['Singing', 'Imagination', 'Tap dancing'],
-        biography: 'Butters Stotch lives in South Park, Colorado, with his overprotective parents, Stephen and Linda. He attends South Park Elementary, where he is frequently bullied. Despite this, Butters stays cheerful, eager to make friends and lend a hand. His naive nature often lands him in amusing predicaments, making him a beloved character in the "South Park" series',
+        biography: 'Butters Stotch, residing in South Park, Colorado, with his overprotective parents, Stephen and Linda, attends South Park Elementary where he faces frequent bullying yet maintains a cheerful demeanor, eagerly seeking friendship and helping others, his naive nature leading to amusing predicaments, endearing him as a beloved character in the "South Park" series.',
         img: "b1.png",
     },
     {
@@ -70,7 +70,7 @@ const teamMembers = [
         strengths: 'Empathy, Patience, Conflict Resolution',
         weaknesses: 'Naive, Overly Simplistic Advice, Personal Insecurities',
         skills: ['Counseling', 'Public Speaking', 'Music'],
-        biography: 'Mr. Mackey is the school counselor at South Park Elementary, where he has worked for many years. He is known for his distinctive catchphrase, "mmkay," and his well-intentioned but sometimes misguided attempts to help the students. Despite his professional role, Mr. Mackey has his own struggles, including a history of substance abuse, which is occasionally referenced in the show. Throughout the series, he provides guidance and support to the students, often with mixed results, but his heart is always in the right place ',
+        biography: 'Mr. Mackey, the school counselor at South Park Elementary, is known for his catchphrase "mmkay" and his earnest but sometimes flawed efforts to help students, yet consistently offering guidance and support with compassion.',
         img: "mm1.png",
     },
     {
@@ -82,7 +82,7 @@ const teamMembers = [
         strengths: 'Versatility, Resilience, Satirical Insight',
         weaknesses: 'Erratic Behavior, Identity Struggles, Extremism',
         skills: ['Teaching', 'Political Maneuvering', 'Adaptability'],
-        biography: 'Mr. Herbert Garrison is a long-time resident and teacher in the small mountain town of South Park, Colorado. Known for his bizarre and often inappropriate behavior, Mr. Garrison has undergone numerous personal transformations, including gender reassignment surgery to become Mrs. Garrison, and later de-transitioning back to Mr. Garrison. His life took a significant turn when he ran for President of the United States, winning the election in a storyline that closely parodied real-world political events. Throughout his tumultuous journey, Mr. Garrison remains a provocative and controversial figure, reflecting the irreverent take on identity, politics, and society at large. Despite his flaws and often offensive antics, he is a central figure in the series, providing both comic relief and sharp social commentary.',
+        biography: 'Mr. Herbert Garrison, a long-time resident and teacher in South Park, Colorado, undergoes personal transformations, including gender reassignment surgery, and unexpectedly wins the presidency in a satirical portrayal of real-world politics, serving as a central figure in the series for his eccentric behavior and sharp social commentary.',
         img: "mg1.png",
     },
     {
@@ -94,7 +94,7 @@ const teamMembers = [
         strengths: 'Wisdom, Musical Talent, Culinary SKills',
         weaknesses: 'Gullibility, etc (ones I cannot state)',
         skills: ['Cooking', 'Singing', 'Mentoring'],
-        biography: 'Jerome "Chef" McElroy is the beloved school cafeteria chef in the town of South Park, Colorado. Known for his soulful singing voice and his affectionate term "children" for addressing the kids, Chef is a central figure who often provides the boys—Stan, Kyle, Cartman, and Kenny—with advice and support. Despite his jovial and caring nature, Chef is also characterized by his strong libido, frequently singing about love and sex, which provides much of the humor. His character was written out of the series in a controversial episode, where it was revealed that Chef had been brainwashed by a cult, leading to his untimely demise. Despite his departure, Chef remains a memorable and iconic character in the South Park universe.',
+        biography: 'Chef, the beloved school cafeteria chef in South Park, Colorado, known for his soulful singing and affection for the kids, provides advice while his humorous songs about love and sex add to his character, though his departure, due to being brainwashed by a cult, remains controversial yet memorable in the series.',
         img: "c1.png",
     },
     {
@@ -106,7 +106,7 @@ const teamMembers = [
         strengths: 'Passionate, Physical Strength, Leadership',
         weaknesses: 'Overzealousness, Aggression, Hypocrisy',
         skills: ['Enforcement of Policies', 'Physical Fitness', 'Public Speaking'],
-        biography: 'PC Principal, whose full name is not revealed in the series, arrives in South Park as the new principal of South Park Elementary, replacing Principal Victoria. He is a staunch advocate for political correctness and takes his role seriously, often going to extreme lengths to ensure that the students and faculty adhere to his strict standards of social justice and sensitivity. His tenure is marked by his aggressive approach to handling issues of discrimination, bullying, and offensive behavior. Despite his often abrasive methods, PC Principal occasionally shows a more nuanced understanding of the complexities of social issues. He becomes a prominent figure in South Park, embodying the satirical take on the sometimes overbearing nature of political correctness in contemporary society.',
+        biography: 'PC Principal arrives at South Park Elementary, advocating for political correctness with aggressive tactics yet occasionally revealing a nuanced understanding of social issues, embodying the shows satire on contemporary PC culture.',
         img: "pcp1.webp",
     },
     {
@@ -118,7 +118,7 @@ const teamMembers = [
         strengths: 'Versatility, Enthusiasm, Resourcefulness',
         weaknesses: 'Impulsiveness, Obsession, Ignorance',
         skills: ['Geology', 'Entrepreneurship', 'Musical Talent'],
-        biography: 'Randy S. Marsh is a geologist and the eccentric father of Stan Marsh, living in the small town of South Park, Colorado. Known for his impulsive and often ridiculous behavior, Randy frequently finds himself embroiled in a variety of outlandish and humorous situations. From becoming a pop star (Lorde) to running Tegridy Farms, his marijuana business, Randy exploits are marked by his boundless enthusiasm and lack of foresight. Despite his many flaws and frequent embarrassments, Randy remains a central figure in his family and the South Park community. His actions, though often misguided, reflect the satirical take on the trials and tribulations of the average American middle-aged man.',
+        biography: 'Randy Marsh, the eccentric father in South Park, Colorado, embarks on impulsive and often absurd adventures, from posing as a pop star to running a marijuana business, reflecting the shows satire on the trials of middle-aged men while remaining a central figure in his family and community.',
         img: "rm1.png",
     },
 ]
@@ -159,7 +159,16 @@ function generateTeamCards(){
                 </div>
                 <div class="card-body">
                 <img src="${member.img}">
-                    <p><strong>Age:</strong> ${member.age}</p>
+                    <strong>Age:</strong> ${member.age}
+                    <ul>
+                        <li><strong>Height:</strong> ${member.height}</li>
+                        <li><strong>Weight:</strong> ${member.weight}</li>
+                        <li><strong>Purpose:</strong> ${member.purpose}</li>
+                        <li><strong>Strengths:</strong> ${member.strengths}</li>
+                        <li><strong>Weaknesses:</strong> ${member.weaknesses}</li>
+                        <li><strong>Skills:</strong> ${member.skills}</li>
+                    </ul>
+                    <strong>Bio:</strong> ${member.biography}
                 </div>
             </div>
     
